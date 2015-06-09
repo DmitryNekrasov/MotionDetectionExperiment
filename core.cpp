9627639039dadd71507aea8804c6f8cb24f8fff8
DmitryNekrasov <scannorone@gmail.com>
2015-06-09 19:10:15 +0600

    вывод видео на экран

--- core.cpp
+++ core.cpp
@@ -6,8 +6,32 @@
 #include <opencv2/imgproc/imgproc_c.h>
 
 void Core::start() {
-    cv::Mat ferrariImage = cv::imread("/Users/ScanNorOne/Desktop/1.jpg");
-    cv::imshow("Ferrari", ferrariImage);
+
+//    // для проверки
+//    cv::Mat ferrariImage = cv::imread("/Users/ScanNorOne/Desktop/1.jpg");
+//    cv::imshow("Ferrari", ferrariImage);
+
+    // создание объекта видеозахвата и открытие (с камеры)
+    cv::VideoCapture video = cv::VideoCapture();
+    video.open(0);
+
+    cv::namedWindow("Motion", 1);
+
+    cv::Mat frame;
+
+    while (true) {
+        video.operator >>(frame);
+        cv::imshow("Motion", frame);
+
+        // выходим из цикла, если нажата какая-нибудь клавиша
+        int keyCode = cv::waitKey(10);
+        if (keyCode >= 0)
+            break;
+    }
+
+    video.release();
+    frame.release();
+    cv::destroyWindow("Motion");
 }
 
 Core::Core()
