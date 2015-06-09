7b7b933d198b058d0d4b249275020711d28caa3d
DmitryNekrasov <scannorone@gmail.com>
2015-06-09 18:32:37 +0600

    загрузка изображения и его отображение

--- core.cpp
+++ core.cpp
@@ -1,8 +1,13 @@
 #include "core.h"
 #include <QDebug>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
 
 void Core::start() {
-    qDebug() << "Done!";
+    cv::Mat ferrariImage = cv::imread("/Users/ScanNorOne/Desktop/1.jpg");
+    cv::imshow("Ferrari", ferrariImage);
 }
 
 Core::Core()
